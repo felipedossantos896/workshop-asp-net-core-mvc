29d374e28c4f86d9c2392cbabcffe9b6b53db5cd
Felipe Carneiro <felipedossantos896@gmail.com>
2020-01-10 12:20:59 -0300

    Foreign key not null (referential integrity)

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -26,6 +26,7 @@ namespace SalesWebMvc.Services
 
         public void Insert(Seller seller)
         {
+            seller.Department = _context.Department.First();
             _context.Add(seller);
             _context.SaveChanges();
         }
